e2638f4b0b07a328652e6971a039cc2ec0ee74ad
bigfa <jigoulee@gmail.com>
2016-02-28 19:00:00 +0800

    chore: update demo

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 !function(){
   'use strict';
+  var ua = navigator.userAgent;
+  var os = '';
+  if ( ua.indexOf('Windows') > -1 ) {
+    os = 'windows';
+  } else {
+    os = 'other';
+  }
+  document.querySelector('body').classList.add(os);
 }();
